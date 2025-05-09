46fc58407a2da3a63775a7ffc560beaaffca8032
Janki-Gauswami <156094604+Janki-Gauswami@users.noreply.github.com>
2025-05-09 10:21:33 +0530

    Update assignment_4_a.py

--- assignment_4_a.py
+++ assignment_4_a.py
@@ -1,3 +1,8 @@
-with open("sample.txt") as f:
-    for x in f:
-        print(x)
+
+    try:
+        with open("sample.txt","r") as f:
+            for x in f:
+                print(x)  
+    except FileNotFoundError:
+        print("Error: The file 'sample.txt' was not found.")
+
